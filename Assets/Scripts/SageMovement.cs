938ae690b53ab352842ae51c4be4659572a88eee
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-10-27 00:04:04 -0400

    Worked on Simple Sage/Guide Floating Animation
    Created draft of particle idle float animation
    Made particle child of camera so that it always appears in the same part of the screen

---
+++ Assets/Scripts/SageMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SageMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}

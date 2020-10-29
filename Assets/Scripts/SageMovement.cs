2eb2d831ba66577fd05b7e6b4b0f6b38237a49c8
YenielDiaz <yenie@LAPTOP-KARDRO8G>
2020-10-29 10:53:04 -0400

    Made Script that allows you to drag the sage

--- Assets/Scripts/SageMovement.cs
+++ Assets/Scripts/SageMovement.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class SageMovement : MonoBehaviour
 {
+    private Camera arCamera;
+    private Rigidbody rb;
 
     void Start()
     {
-
+        arCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
+    void FixedUpdate() {
 
-    void Update()
-    {
+    }
         
-    }
+      
+  
 }

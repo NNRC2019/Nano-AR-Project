53a767dbffc3798bbe23169d2c5996edfb5ee72e
Geidel Xavier <geidel.solivan@upr.edu>
2020-10-30 16:57:57 -0400

    Initial Upload
    Added minigame templates and "tap for menu" functionality

---
+++ Assets/Scenes/StationButtonBehaviors.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationButtonBehaviors : MonoBehaviour
+{
+    
+    public void Level1(){
+        //SceneTransition
+    }
+
+    public void Level2()
+    {
+        //SceneTransition
+    }
+
+    public void Level3()
+    {
+        //SceneTransition
+    }
+    public void Exit(){
+        Application.Quit();
+    }
+}

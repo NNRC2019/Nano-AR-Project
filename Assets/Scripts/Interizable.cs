ccde65540a67977c39b5e943bc02d120fda25ea7
NatiPerez459 <70607250+NatiPerez459@users.noreply.github.com>
2020-10-30 10:19:57 -0400

    Dialogue
    Sample Dialogue scene without a instantiate

---
+++ Assets/Scripts/Interizable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interizable : MonoBehaviour
+{
+    //Shows Dialogue on Unity in Test Button
+    public Dialogue dialogue;
+
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}

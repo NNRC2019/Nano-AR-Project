1e525e0b42b1f2eee2761c028f4f01d9266071f0
NatiPerez459 <70607250+NatiPerez459@users.noreply.github.com>
2020-11-01 01:10:10 -0400

    Dialogue
    Began progress to instantiate dialogue when sage is clickes, currently dialogue pops automatically and initiates text when sage is clicked

---
+++ Assets/Scripts/UIAppearClic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppearClic : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+
+   private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            customImage.enabled = false;
+        }
+    }
+
+}
+

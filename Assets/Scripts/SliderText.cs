e63729fa471847d5a5845659bf89ca84675eb296
NatiPerez459 <70607250+NatiPerez459@users.noreply.github.com>
2020-11-12 00:38:44 -0400

    Slider Trial
    Text Speed slider

---
+++ Assets/Scripts/SliderText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+
+    Text percentageText;
+
+    public Slider speed;
+    public int cps;
+
+
+
+    void Start()
+    {
+        cps = 7;
+        percentageText = GetComponent<Text>();
+        speed = GetComponent<Slider>();
+        cps = (int)speed.value;
+    }
+
+    // Update is called once per frame
+    public void textUpdate(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 10) + "%";
+        cps = (int)speed.value;
+
+    }
+
+}

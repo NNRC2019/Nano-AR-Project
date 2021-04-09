a53b88efc228a7401b6669b8ab4d006e73dfc038
YenielDiaz <31051936+YenielDiaz@users.noreply.github.com>
2021-04-08 20:10:02 -0400

    Added special UI to test Maze
    Added UI to show curren x,y,z coordinates of Maze Object after instantiating and buttons to move the Maze object around

---
+++ Assets/Scripts/MazeScriptsz/ButtonManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    
+    GameObject targetToMove;
+    [SerializeField] float moveSpeed = 1f;
+
+
+    void SetTarget()
+    {
+        targetToMove = FindObjectOfType<Maze>().gameObject; //For now we are targeting the Maze object when it gets instantiated
+    }
+ 
+
+    public void MoveUp()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(0, moveSpeed, 0);
+        }
+       
+    }
+
+    public void MoveDown()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(0, -moveSpeed, 0);
+        }
+        
+    }
+
+    public void MoveLeft()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(-moveSpeed, 0, 0);
+        }
+        
+    }
+
+    public void MoveRight()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(moveSpeed, 0, 0);
+        }
+        
+    }
+
+    public void MoveForward()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(0, 0, moveSpeed);
+        }
+    }
+
+
+    public void MoveBack()
+    {
+        if (targetToMove == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            targetToMove.transform.position += new Vector3(0, 0, -moveSpeed);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/MazeScriptsz/LocationTextManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationTextManager : MonoBehaviour
+{
+    
+    [SerializeField] Text xPosText;
+    [SerializeField] Text yPosText;
+    [SerializeField] Text zPosText;
+
+    GameObject target;
+    
+    void Update()
+    {
+        if(target == null)
+        {
+            SetTarget();
+        }
+        else
+        {
+            if(xPosText != null) xPosText.text = $"X : {target.transform.position.x}";
+            if(yPosText != null) yPosText.text = $"Y : {target.transform.position.y}";
+            if(zPosText != null) zPosText.text = $"Z : {target.transform.position.z}";
+        }
+    }
+
+    void SetTarget()
+    {
+        target = FindObjectOfType<Maze>().gameObject; //For now we are targeting the Maze object when it gets instantiated
+    }
+}

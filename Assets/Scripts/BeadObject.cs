cfec79791c32ff09b6fb69ec9afce4f92407aea4
Geidel Xavier <geidel.solivan@upr.edu>
2021-04-22 20:34:55 -0400

    Added bead object and player movement scripts

---
+++ Assets/Scripts/BeadObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script contains all properties and methods pertaining
+/// to the bead object.
+/// </summary>
+public class BeadObject : MonoBehaviour
+{
+
+    [SerializeField] GameObject molecule;
+    private bool isUV = false;
+
+    /// <summary>
+    /// Start function designates "isUV" property to True or
+    /// False based on a 7:3 ratio. (70% true : 30% false)
+    /// </summary>
+    public void Start()
+    {
+        int num = Random.Range(1, 11);
+        if (num > 3)
+        {
+            isUV = true;
+        }
+    }
+
+    /// <summary>
+    /// Determines if bead is UV by accessing its boolean property.
+    /// </summary>
+    /// <returns>Returns a boolean based on isUV property.</returns>
+    public bool IsUV()
+    {
+        return isUV;
+    }
+
+    /// <summary>
+    /// Method will call molecule property and display it
+    /// for inspection by player.
+    /// </summary>
+    public void Inspect()
+    {
+        //ToDo call molecule property
+    }
+
+    /// <summary>
+    /// Method changes color of bead object to a lighter version
+    /// if it is categorized as UV bead.
+    /// </summary>
+    public void Shine()
+    {
+        if (isUV)
+        {
+            var beadMaterial = this.GetComponent<Renderer>().material;
+            beadMaterial.color *= 1.5f;
+        }
+    }
+
+    /// <summary>
+    /// Method changes color of bead object to a darker version
+    /// to reverse the shine effect.
+    /// </summary>
+    public void UnShine()
+    {
+        if (isUV)
+        {
+            var beadMaterial = this.GetComponent<Renderer>().material;
+            beadMaterial.color /= 1.5f;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script contains the variables and methods to allow player movement. The "AR Session Origin"
+/// is treated as the player and the "AR Camera" is treated as the player view.
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+
+    GameObject player;
+    GameObject view;
+    [SerializeField] float moveSpeed = 1f;
+
+    /// <summary>
+    /// Method assigns "AR Session Origin" to player property and "AR Camera" to view property.
+    /// </summary>
+    void SetTarget()
+    {
+        player = GameObject.Find("AR Session Origin");
+        view = GameObject.Find("AR Camera");
+    }
+
+    /// <summary>
+    /// Method calculates a new position for the player based on where they are looking and, if 
+    /// it is within the allowed height, assigns this new position to the player.
+    /// </summary>
+    public void MoveForward()
+    {
+        if (player == null)
+        {
+            SetTarget();
+        }
+
+        var newPosition = player.transform.position + view.transform.forward * moveSpeed;
+
+        //-1 and 2 are subject to change depending on allowed height.
+        if (newPosition.y > -1 && newPosition.y < 2)
+        {
+            player.transform.position = newPosition;
+        }
+    }
+}
